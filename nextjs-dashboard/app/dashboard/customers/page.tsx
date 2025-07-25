575737a914a51022f1e17e20a4711dbebd70f2ce
brooker <mubbala9@gmail.com>
2025-07-25 17:50:09 +0300

    w3

---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+// filepath: c:\Next_Dashboard\nextjs-dashboard\app\dashboard\customers\page.tsx
+export default function Page() {
+  return <p>Customers Page</p>;
+}
---
+++ nextjs-dashboard/app/dashboard/invoice/page.tsx
@@ -0,0 +1,4 @@
+// filepath: c:\Next_Dashboard\nextjs-dashboard\app\dashboard\invoices\page.tsx
+export default function Page() {
+  return <p>Invoices Page</p>;
+}
